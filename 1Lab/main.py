39864f2591f4a0b0e588feae1af82bc8fa73bb70
hyaluronic <60736298+hyaluronic@users.noreply.github.com>
2022-09-18 16:58:42 +0300

    1Lab: graph and report

--- 1Lab/main.py
+++ 1Lab/main.py
@@ -1,6 +1,11 @@
 import random
 
 import numpy as np
+from matplotlib import pyplot as plt
+
+bisection_method_points = []
+golden_section_method_points = []
+newtons_method_points = []
 
 def bisection_method(func, l, r, eps):
     def f(x):
@@ -42,6 +47,8 @@ def bisection_method(func, l, r, eps):
             r = x2
             L = r - l
 
+        bisection_method_points.append([l, r])
+
         # 6.
         if L < eps:
             break
@@ -49,8 +56,8 @@ def bisection_method(func, l, r, eps):
     print("***********************")
     print("Bisection method")
     print("Number of iterations: %d" % i)
-    print("f(xm) =", fxm)
-    print("xm =", xm)
+    print("f(Xmin) =", fxm)
+    print("Xmin =", xm)
     print("Number of functions calculated: %d" % counter)
     print("***********************")
 
@@ -87,6 +94,8 @@ def golden_section_method(func, l, r, eps, tau):
             fx1 = f(x1)
             counter += 1
 
+        golden_section_method_points.append([l, r])
+
         # 4.
         if L < eps:
             break
@@ -94,17 +103,10 @@ def golden_section_method(func, l, r, eps, tau):
     print("***********************")
     print("Golden section method")
     print("Number of iterations: %d" % i)
-    findMin(fx1, fx2, x1, x2)
+    print("f(Xmin) =", fx1 if fx1 < fx2 else fx2)
+    print("Xmin =", x1 if fx1 < fx2 else x2)
     print("Number of functions calculated: %d" % counter)
     print("***********************")
-
-def findMin(fx1, fx2, x1, x2):
-    if fx1 < fx2:
-        print("f(x1) =", fx1)
-        print("x1 =", x1)
-    else:
-        print("f(x2) =", fx2)
-        print("x2 =", x2)
 
 def newtons_method(func, x0, eps):
     counter = 0
@@ -112,6 +114,7 @@ def newtons_method(func, x0, eps):
     for i in range(1, 100):
         xn = x0 - first_deriv(x0, func) / second_deriv(x0, func)
         counter += 2
+        newtons_method_points.append(xn)
         if abs(xn - x0) < eps:
             break
         x0 = xn
@@ -119,8 +122,8 @@ def newtons_method(func, x0, eps):
     print("***********************")
     print("Newton's method")
     print("Number of iterations: %d" % i)
-    print("f(xn): %f" % f(xn, func))
-    print("xn: %f" % xn)
+    print("f(Xmin): %f" % f(xn, func))
+    print("Xmin: %f" % xn)
     print("Number of functions calculated: %d" % counter)
     print("***********************")
 
@@ -135,19 +138,66 @@ def second_deriv(x, func):
     h = 1e-5
     return (first_deriv(x + h, func) - first_deriv(x, func)) / h
 
+def drawGraph(func, l, r, char):
+    x = np.linspace(l, r, 100)
+    y = eval(func)
+
+    print("bisection: ", bisection_method_points)
+    print("golden: ", golden_section_method_points)
+    print("newtons: ", newtons_method_points)
+
+    fig1 = plt.figure()
+    ax = fig1.add_subplot(1, 1, 1)
+    ax.set(ylim=(-2, 5))
+    ax.spines['left'].set_position('zero')
+    ax.spines['bottom'].set_position('zero')
+    ax.spines['right'].set_color('none')
+    ax.spines['top'].set_color('none')
+    ax.xaxis.set_ticks_position('bottom')
+    ax.yaxis.set_ticks_position('left')
+
+    if char == 'b':
+        drawPoints(bisection_method_points)
+    elif char == 'g':
+        drawPoints(golden_section_method_points)
+    elif char == 'n':
+        drawPoints(newtons_method_points)
+
+    plt.plot(x, y, 'r')
+    plt.show()
+
+def drawPoints(point_array):
+    show = [1, 2, 3, 4, len(point_array)]
+    for i in range(0, len(point_array)):
+        r = random.random()
+        b = random.random()
+        g = random.random()
+        a = 1
+        color = (r, g, b, a)
+        if i+1 in show:
+            if isinstance(point_array[i], list):
+                plt.scatter(point_array[i][0], 0, color=color)
+                plt.annotate(i + 1, (point_array[i][0], 0))
+                plt.scatter(point_array[i][1], 0, color=color)
+                plt.annotate(i + 1, (point_array[i][1], 0))
+            else:
+                plt.scatter(newtons_method_points[i], 0, color=color)
+                plt.annotate(i + 1, (newtons_method_points[i], 0))
+
 def main():
-    # function = "((x ** 2 - 3) ** 2) / 9 - 1"
-    # l = 0
-    # r = 10
-    function = "(100 - x)**2"
-    l = 60
-    r = 150
+    function = "((x ** 2 - 3) ** 2) / 9 - 1"
+    l = 0
+    r = 10
+    # function = "(100 - x) ** 2"
+    # l = 60
+    # r = 150
     eps = 0.0001
     bisection_method(function, l, r, eps)
     tau = 0.61803
     golden_section_method(function, l, r, eps, tau)
     x0 = 5
-    newtons_method(function, x0, 0.0001)
+    newtons_method(function, x0, eps)
+    drawGraph(function, 0, 4, 'n')
 
 if __name__ == '__main__':
     main()
